d5dd304cf25f243e19c3bd0a49b3e2d437cecaaf
Kaopong Sangkittiyakorn <84172946+kuisskui@users.noreply.github.com>
2025-05-29 23:10:29 +0700

    change project name

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from typing import List
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Movie App"
+    PROJECT_NAME: str = "Movie Application"
     SQLALCHEMY_DATABASE_URL: str = "sqlite:///./movie_app.db"
     CORS_ORIGINS: List[str] = ["*"]
     model_config = SettingsConfigDict(env_file=".env")
